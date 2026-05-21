f120f151b03caf41618f0c8072d1cc1803e6afde
joshmcg05 <joshmcg05@icloud.com>
2026-05-21 15:26:25 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
 const slider = document.querySelector('.slider');
 
+const scrollAmount = 344;
+
+/* NEXT BUTTON */
+
 document.querySelector('.next').addEventListener('click', () => {
-  slider.scrollBy({
-    left: 400,
-    behavior: 'smooth'
-  });
+
+  if (
+    slider.scrollLeft + slider.clientWidth >=
+    slider.scrollWidth - 10
+  ) {
+
+    slider.scrollTo({
+      left: 0,
+      behavior: 'smooth'
+    });
+
+  } else {
+
+    slider.scrollBy({
+      left: scrollAmount,
+      behavior: 'smooth'
+    });
+
+  }
+
 });
 
+/* PREVIOUS BUTTON */
+
 document.querySelector('.prev').addEventListener('click', () => {
-  slider.scrollBy({
-    left: -400,
-    behavior: 'smooth'
-  });
+
+  if (slider.scrollLeft <= 0) {
+
+    slider.scrollTo({
+      left: slider.scrollWidth,
+      behavior: 'smooth'
+    });
+
+  } else {
+
+    slider.scrollBy({
+      left: -scrollAmount,
+      behavior: 'smooth'
+    });
+
+  }
+
 });
